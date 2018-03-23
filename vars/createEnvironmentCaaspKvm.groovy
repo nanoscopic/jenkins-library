0aebe72cb34d41f1d1ef585f3d5dcafd38e40269
Maximilian Meister <mmeister@suse.de>
2018-03-23 13:27:52 +0100

    enable testing packages from the sandbox repo
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- vars/createEnvironmentCaaspKvm.groovy
+++ vars/createEnvironmentCaaspKvm.groovy
@@ -42,13 +42,18 @@ Environment call(Map parameters = [:]) {
         velumImage = "${options.velumImage}"
     }
 
+    def extraRepo = ""
+    if (options.extraRepo) {
+        extraRepo = "--extra-repo ${options.extraRepo}"
+    }
+
     timeout(120) {
         dir('automation/caasp-kvm') {
             withCredentials([
                 string(credentialsId: 'caasp-proxy-host', variable: 'proxy'),
                 string(credentialsId: 'caasp-location', variable: 'location')
             ]) {
-                sh(script: "set -o pipefail; ./caasp-kvm -P ${proxy} -L ${location} ${vanillaFlag} --build --disable-meltdown-spectre-fixes -m ${masterCount} -w ${workerCount} --image ${options.image} --velum-image ${velumImage} --admin-ram ${options.adminRam} --admin-cpu ${options.adminCpu} --master-ram ${options.masterRam} --master-cpu ${options.masterCpu} --worker-ram ${options.workerRam} --worker-cpu ${options.workerCpu} 2>&1 | tee ${WORKSPACE}/logs/caasp-kvm-build.log")
+                sh(script: "set -o pipefail; ./caasp-kvm -P ${proxy} -L ${location} ${vanillaFlag} --build --disable-meltdown-spectre-fixes -m ${masterCount} -w ${workerCount} --image ${options.image} --velum-image ${velumImage} --admin-ram ${options.adminRam} --admin-cpu ${options.adminCpu} --master-ram ${options.masterRam} --master-cpu ${options.masterCpu} --worker-ram ${options.workerRam} --worker-cpu ${options.workerCpu} ${extraRepo} 2>&1 | tee ${WORKSPACE}/logs/caasp-kvm-build.log")
             }
 
             // Read the generated environment file
